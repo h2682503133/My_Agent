2608415844bf922d718e0aab72369ec1d5251abf
h2682503133 <2195239690@qq.com>
2026-03-29 04:28:35 +0800

    添加了日志与启动

---
+++ log_client.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 5001))
+
+print("=== 模型对话独立日志窗口 ===")
+while True:
+    data = s.recv(4096)
+    if not data:
+        break
+    print(data.decode("utf-8"), end="")
